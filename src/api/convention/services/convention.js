ef658492d0185a80998e46e2094d36b78af3680e
AlVanilla <al.bomediano@obf.ateneo.edu>
2022-04-12 16:57:44 +0800

    added conventions collection type

---
+++ src/api/convention/services/convention.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * convention service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::convention.convention');
